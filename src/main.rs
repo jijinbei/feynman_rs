4544724432d03640018d2314ecf95d06cbb0a24a
jijinbei <hikoharu14142@outlook.jp>
2024-10-03 04:03:05 +0900

    Refactor diagram definition in main.rs to include vertex and interaction

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ pub struct FeynmanParser;
 
 fn main() {
     let diagram = "vertex photon1 photon2 axion
-axion ~> photon1
-axion ~> photon2";
+vertex point
+
+axion -> pointSD
+point ~p~ photon1
+point ~p~ photon2
+";
 
     let successful_parse = FeynmanParser::parse(Rule::diagram, diagram).unwrap();
     println!("{:?}", successful_parse);
